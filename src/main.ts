37b41e8ee0c240007dd1a811363bbf95b99f9022
David Almeida <contato.davidalmeida@outlook.com>
2025-08-27 16:35:17 -0300

    👔 Update business logic

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,18 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-`;
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML = ``;
+
+const collectionFilterNav = document.getElementById('collection-filter');
+
+collectionFilterNav?.addEventListener('click', (e) => {
+  const target = e.target as HTMLElement;
+  const selectedButton = target.closest('button');
+
+  if (!selectedButton) return;
+
+  document.querySelectorAll('.menu-btn').forEach((btn) => {
+    btn.classList.remove('border-neutral-900');
+    btn.classList.add('border-neutral-50');
+  });
+
+  selectedButton.classList.remove('border-neutral-50');
+  selectedButton.classList.add('border-neutral-900');
+});
